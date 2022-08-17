8a9adac3a518e0b9f2a89107e83ed8eb698d4eb9
Kimgmltn <Kimgmltn0323@gmail.com>
2022-08-18 00:03:09 +0900

    통신부분 공통으로 빼기

---
+++ js/http.js
@@ -0,0 +1,47 @@
+const post = function(url, param){
+    const httpRequest =new XMLHttpRequest();
+    let response = false;
+    
+    httpRequest.open('POST', url, false);
+
+    httpRequest.onreadystatechange = () => {
+        if(httpRequest.readyState === XMLHttpRequest.DONE){
+          if(httpRequest.status === 200){
+              response = true;
+          }
+        }
+      }
+
+    httpRequest.setRequestHeader('Content-type', 'application/json')
+    httpRequest.setRequestHeader('Cache-Control', 'no-cache')
+    httpRequest.send(param);
+
+    return response;
+  }
+
+
+
+
+
+const get = function(url, param){
+    const httpRequest =new XMLHttpRequest();
+    let response;
+
+    httpRequest.open('GET', url, false);
+     httpRequest.onreadystatechange =  () => {
+      if(httpRequest.readyState === XMLHttpRequest.DONE){
+        if(httpRequest.status === 200){
+          response = JSON.parse(httpRequest.response);
+        }
+      }
+    }
+    
+    if(param){
+      httpRequest.send(param);
+      
+    }else{
+      httpRequest.send();
+    }
+
+    return response;
+}
